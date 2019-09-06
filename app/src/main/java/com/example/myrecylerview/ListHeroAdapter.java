f7d6980abbef36e6c86f99f6f4e9ef15f09f25ca
Nioni <artha.putri19@gmail.com>
2019-09-06 15:13:33 +0700

    Tugas recycle view selesai

---
+++ app/src/main/java/com/example/myrecylerview/ListHeroAdapter.java
@@ -0,0 +1,63 @@
+package com.example.myrecylerview;
+
+import android.app.Dialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
+public class ListHeroAdapter extends RecyclerView.Adapter<ListHeroAdapter.ListViewHolder> {
+
+    private ArrayList<Hero> listero;
+
+    public ListHeroAdapter(ArrayList<Hero> list){
+        this.listero = list;
+    }
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_row_hero, viewGroup, false);
+        return new ListViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListViewHolder holder, int position) {
+        Hero hero = listero.get(position);
+
+        Glide.with((holder.itemView.getContext()))
+                .load(hero.getPhoto())
+                .apply(new RequestOptions().override(55, 55))
+                .into(holder.imgPhoto);
+
+        holder.tvName.setText(hero.getName());
+        holder.tvDetail.setText(hero.getDetail());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listero.size();
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgPhoto;
+        TextView tvName, tvDetail;
+
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgPhoto = itemView.findViewById(R.id.img_item_photo);
+            tvName = itemView.findViewById(R.id.tv_item_name);
+            tvDetail = itemView.findViewById(R.id.tv_item_detail);
+        }
+    }
+
+}
